4eb332b3ee78ec1390aa0385af3b0a7f5d3bd538
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-12-29 16:56:02 -0500

    finished sample map, character movement, nav mesh implemented, as well as scroll wheel.

---
+++ Prophecy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))] 
+public class PlayerController : MonoBehaviour
+{
+    Camera cam;
+
+    // to avoid walking on non-walkable objects
+
+    public LayerMask movementMask;
+
+    PlayerMotor motor;
+
+
+
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("Left mouse hit:  " + hit.collider.name + " " + hit.point);
+
+                motor.MoveToPoint(hit.point);
+            }
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log("Right mouse hit:  " + hit.collider.name + " " + hit.point);
+
+                // check if we hit an interactive object. Set as focus.
+            }
+        }
+    }
+}
---
+++ Prophecy/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// unity automatically adds nav mesh agent when using this component
+[RequireComponent(typeof(NavMeshAgent))] 
+public class PlayerMotor : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+}
