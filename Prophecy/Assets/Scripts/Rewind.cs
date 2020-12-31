9255d17786f285f6a4221b7c3a0c8766b6249cf0
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-12-31 10:02:43 -0500

    added functionality to rewind boss by hitting the enter key.

---
+++ Prophecy/Assets/Scripts/Rewind.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rewind : MonoBehaviour
+{
+    public bool isRewinding = false;
+
+    public Text text;
+
+    List<Vector3> positions;
+
+    // can add visible UI that it is rewinding
+
+    void Start()
+    {
+        positions = new List<Vector3>(); // different positions in point of time
+        text.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        if(isRewinding)
+            ActionRewind();
+        else
+            Record();
+    }
+
+    void ActionRewind()
+    {
+        if(positions.Count > 0)
+        {
+            transform.position = positions[0];
+            positions.RemoveAt(0);
+        }
+        else
+        {
+            StopRewind();
+        }
+
+        
+    }
+
+    void Record()
+    {
+        positions.Insert(0, transform.position);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+            StartRewind();
+        if(Input.GetKeyUp(KeyCode.Return))
+            StopRewind();
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+        text.enabled = true;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+        text.enabled = false;
+    }
+}
